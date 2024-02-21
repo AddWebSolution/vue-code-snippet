5b1be1cff93a1455ecd97600a8862fca014c165f
Avinash Z <avinash@addwebsolution.in>
2024-02-21 17:53:46 +0530

    Click Outside Directive Added

---
+++ click-outside/README.md
@@ -0,0 +1,5 @@
+# vue-code-snippet for click-outside
+
+## This vue directive triggers the defined method when click action happens outside the bind element.
+
+
---
+++ click-outside/clickOutside.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-clickoutside="outsideClicked">Click Outside of me....</div>
+</template>
+
+<script>
+Vue.directive('clickoutside', {
+    bind (el, { value }) {
+        if (typeof value !== 'function') {
+            console.warn(`Function Expected, got ${value}`)
+            return
+        }
+
+        document.addEventListener('click', e => el.contains(e.target) || value())
+    }
+})
+
+export default {
+    methods: {
+        outsideClicked () {
+            // Write your logic here
+        }
+    }
+}
+</script>
